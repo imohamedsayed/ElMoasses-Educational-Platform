69578931994f4e60ac10da31fe70cdc3eab93cc3
Mohamed Sayed <mohamedelsayed0901@gmailcom>
2023-12-06 14:14:10 +0200

    project startup

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,17 @@
-import { createStore } from 'vuex'
-
+import { createStore } from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import * as cookies from "js-cookie";
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+  plugins: [
+    createPersistedState({
+      getState: (key) => cookies.getJSON(key),
+      setState: (key, state) =>
+        cookies.set(key, state, { expires: 3, secure: true }),
+    }),
+  ],
+});
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script>
+import { defineComponent } from "vue";
+
+// Components
+
+export default defineComponent({
+  name: "HomeView",
+  components: {},
+});
+</script>
