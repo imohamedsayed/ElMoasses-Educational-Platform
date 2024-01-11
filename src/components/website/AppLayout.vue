daf50caa0abe5c527db359a379f7162510eee2f8
Mohamed Sayed <mohamedelsayed0901@gmailcom>
2024-01-12 00:05:45 +0200

    Integrate tawkTo chat to the project

--- src/components/website/AppLayout.vue
+++ src/components/website/AppLayout.vue
@@ -11,13 +11,21 @@
   </v-app>
 </template>
 
-<script>
+<script setup>
 import NavigationDrawer from "./layout/NavigationDrawer.vue";
 import AppNav from "./layout/AppNav.vue";
 import AppFooter from "./layout/AppFooter.vue";
-export default {
-  components: { NavigationDrawer, AppNav, AppFooter },
-};
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+(function () {
+  var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = "https://embed.tawk.to/65a05e2b8d261e1b5f521865/1hjt6vmk4";
+  s1.charset = "UTF-8";
+  s1.setAttribute("crossorigin", "*");
+  s0.parentNode.insertBefore(s1, s0);
+})();
 </script>
 
 <style></style>
